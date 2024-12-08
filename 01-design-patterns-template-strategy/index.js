dff3d618cfec904ce39603ae21bb60e6277978fd
Daniela <danielarodrigues2po@gmail.com>
2024-12-08 18:05:17 -0300

    Implementação Facade

--- 01-design-patterns-template-strategy/index.js
+++ 01-design-patterns-template-strategy/index.js
@@ -1,21 +1,12 @@
-import sys from 'node:sys';
 
-import FormaterHTML from './src/FormaterHTML.js';
-import FormaterTXT from './src/FormaterTXT.js';
-import CitiesReporter from './src/CitiesReporter.js';
+import ClasseFacade from './src/Facade.js'
 
-const [cmd, script, param1] = process.argv,
-      filename = './data/cidades-2.json';
+const [cmd, script, param1] = process.argv
+console.log(param1)
+const facade = new ClasseFacade()
 
-const formaterStrategies = {
-  'html': new FormaterHTML(),
-  'txt': new FormaterTXT()
-};
-
-let reporter = new CitiesReporter({
-      formaterStrategy: formaterStrategies[param1]
-    }),
-    output = reporter.report(filename);
+facade.defineEstrategia(param1)
+output = facade.relatorio()
 
 console.log(output);
 
